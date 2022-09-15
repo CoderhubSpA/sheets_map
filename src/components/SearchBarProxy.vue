b45b48ba788d1280b67d789d7f3177e55544a303
José Vergara <jose.vergara@coderhub.cl>
2022-09-15 11:46:01 -0300

    SAIT-45 Change SearchBarProxy
    Cambia el componente para cargar la barra que se usará de manera
    síncrona o instantánea en lugar de asíncrona o diferida. Esto
    porque se detecto un problema al usar la librería en el proyecto
    de Sheets, dónde no se pudo cargar correctamente el componente a usar.

--- src/components/SearchBarProxy.vue
+++ src/components/SearchBarProxy.vue
@@ -1,22 +1,22 @@
 <template>
   <search
-    :is="component"
+    :is="componentName"
     :config="config"
     @change-location="$emit('change-location', $event)"
   />
 </template>
 
 <script>
+import SAITSearchBar from "./SAITSearchBar.vue";
+
 export default {
   name: "SearchBarProxy",
+  components: {
+    SAITSearchBar,
+  },
   props: {
     componentName: String,
     config: Object,
-  },
-  computed: {
-    component() {
-      return () => import("./" + this.componentName);
-    },
   },
 };
 </script>
