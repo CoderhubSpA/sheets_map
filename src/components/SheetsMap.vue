02487007de88ecad78b554c9d9c8c33c670fc30f
Eduardo Alvarado <eduardoalvara2@gmail.com>
2022-11-08 14:56:57 -0300

    Refactored Supercluster and moved to a new component

--- src/components/SheetsMap.vue
+++ src/components/SheetsMap.vue
@@ -8,7 +8,7 @@
             <!-- https://vue2-leaflet.netlify.app/components/LMap.html#demo -->
             <l-map 
                 @ready="ready()"
-                @moveend="getClusterInfo();"
+                @moveend="onMapMoveEnd();"
                 :zoom.sync="zoom"
                 :center.sync="center"
                 ref="my_map"
@@ -48,44 +48,17 @@
                     ></l-tile-layer>
                 
                     <!--https://vue2-leaflet.netlify.app/components/LCircleMarker.html -->
-                <l-layer-group  ref="lgroup">
-                    <l-marker
-                        v-for="(cluster, index) in clusters"
-                        v-bind:key="index"
-                        :lat-lng="cluster.lat_lng"
-                    >
-                        <l-icon
-                            :icon-anchor="[40,40]"
-                            :class-name="'marker-cluster marker-cluster-'+cluster.size"
-                        >
-                            <div class="headline">
-                                <span> {{cluster.properties.point_count_abbreviated}}</span>
-                            </div>
-                        </l-icon>
-                    </l-marker>
-                    <l-circle-marker
-                        ref="circlemarker"
-                        v-for="(marker, index) in markers"
-                            :key="'marker-' + index"
-                            :lat-lng="marker.lat_lng"
-                            :radius="3"
-                            v-on:click="getMarkerData(marker)" 
-                            color="#00642a"
-                            >
-                        <!-- https://leafletjs.com/reference.html#popup-->
-                        <l-popup :options="popup_point_options" class="marker-pop-up">
-                            <div v-if="marker.has_data" class="marker-pop-up-content">
-                                <div v-for="(col,key) in visible_columns"  :key="'col-' + key" class="marker-pop-up-single-info">
-                                    <span class="marker-pop-up-info-title"> <b>{{col.name}}</b> </span> <br>
-                                    <span class="marker-pop-up-info-content"> {{getPopupData(marker,col)}} </span>
-                                </div>
-                            </div>
-                            <div v-else class="marker-pop-up-single-info">
-                                Cargando...
-                            </div>
-                        </l-popup>
-                    </l-circle-marker>
-                </l-layer-group> 
+                <supercluster-layer
+                    :data="data"
+                    :info="info"
+                    :map="map"
+                    :config="config"
+                    :col_lat="col_lat"
+                    :col_lng="col_lng"
+                    :entity_type_id="entity_type_id"
+                    :base_url="base_url"
+                    ref="analytic_cluster_layer"
+                ></supercluster-layer>   
                 <!-- 
                     Analytic layers 
                         - Analytic Cluster 
@@ -128,12 +101,12 @@
 <script>
 import L from 'leaflet';
 import _ from 'lodash';
-import {LMap, LTileLayer, LLayerGroup, LMarker, LCircleMarker, LPopup, LIcon,LGeoJson, LWMSTileLayer } from 'vue2-leaflet';
+import {LMap, LTileLayer, LMarker, LGeoJson, LWMSTileLayer } from 'vue2-leaflet';
 import SearchBarProxy from './SearchBarProxy.vue';
+import SuperclusterLayer from './layers/SuperclusterLayer.vue';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
 import axios from 'axios';
-import Supercluster from 'supercluster';
 import HeatmapOverlay from'heatmap.js/plugins/leaflet-heatmap'
 import { BButton, BIcon } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -151,16 +124,13 @@ export default {
     components: {
         LMap,
         LTileLayer,
-        LLayerGroup,
-        LCircleMarker,
-        LPopup,
-        LIcon,
         LMarker,
         LGeoJson,
         "l-wms-tile-layer": LWMSTileLayer,
         BButton,
         BIcon,
         SearchBarProxy,
+        SuperclusterLayer,
     },
     props: {
         // Propiedades de componentes
@@ -183,7 +153,6 @@ export default {
     },
     data () {
         return {
-            // map: undefined,
             url: '',
             default_base_layer: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
             default_attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -195,8 +164,9 @@ export default {
             center                    : undefined,
             col_lat                   : undefined,
             col_lng                   : undefined,
-            markers_data              : {},
+            
             map                       : undefined,
+            map_bounds                : undefined,
             circle                    : undefined,
             /*Layers*/
             analytic_cluster          : undefined,
@@ -210,11 +180,10 @@ export default {
             base_open_street_map      : undefined,
             operative_geoserver_wms   : [],
             /*Layers*/
-            clusters_markers          : [],
+            
             bounds_filters            : [],
             num_zoom                  : false,
             bounds                    : [],
-            index                     : [],
             h3                        : require("h3-js"),
             shouldShowSearchMarker    : false,
             searchMarkerLatLng        : null,
@@ -315,104 +284,7 @@ export default {
             };
 
         },
-        markers_latlgn(){
-            
-            if(
-                !this.data.data
-                || !this.col_lat
-                || !this.col_lng
-            ) return [];
-             return [];
-        },
-        visible_columns(){
-            if(!this.info.columns) return [];
-            
-            let all_columns = this.info.columns;
-            let visible_columns = all_columns.filter( c => {
-                if (c.visible == 1) {
-                    return c;
-                }
-            })
-            .filter(d => d);
-            return visible_columns;
-        },
-        geo_json(){
-            // Al recibir data, geo_json se construye a partir de esa data
-            // y las columnas configuradas como lat y lng
-            let geo_json = {type: "FeatureCollection", features: []};
 
-            if(
-                !this.data.data
-                || !this.col_lat
-                || !this.col_lng
-            ) return geo_json;
-
-            geo_json.features = this.data.data.map((d) => {
-                try {
-                    let lat = d[this.col_lat];
-                    let lng = d[this.col_lng];
-
-                    lat = (typeof lat == 'string') ? parseFloat(lat.replace(/,/, '.')):lat;
-                    lng = (typeof lng == 'string') ? parseFloat(lng.replace(/,/, '.')):lng;
-                    
-                    if(!lat || !lng) return;
-    
-                    let coordinates = [lng,lat];
-                    // let coordinates = [marker.lat_lng[1],marker.lat_lng[0]];
-                    return {
-                        type: "Feature",
-                        properties: {
-                            id: d.id,
-                        },
-                        geometry: {
-                            type: "Point",
-                            coordinates: coordinates
-                        }
-                    };
-
-                } catch (error) {
-                    console.error(error);
-                }
-            }).filter( d => d);
-            
-            return geo_json;      
-        },
-        //Supercluster
-        clusters(){
-
-            let clusters = this.clusters_markers.map( d =>{
-                if(d.properties.cluster){
-                
-                    let count = d.properties.point_count;
-                    let size =
-                        count < this.config.sh_map_medium_cluster_size_starts_at ? 'small' :
-                        count < this.config.sh_map_large_cluster_size_starts_at  ? 'medium' : 'large';
-
-                    d.properties.point_count_abbreviated = (d.properties.point_count_abbreviated > 900) ? '900+' : d.properties.point_count_abbreviated;
-                    return {
-                        lat_lng    : [d.geometry.coordinates[1], d.geometry.coordinates[0]],
-                        properties : d.properties,
-                        size       : size,
-                    };
-                }
-            })
-            .filter(d => d);    
-            return clusters;
-        },
-        markers(){
-            let markers = this.clusters_markers.map( d =>{
-                if(!d.properties.cluster){
-                    return {
-                        lat_lng  : [d.geometry.coordinates[1], d.geometry.coordinates[0]],
-                        id       : d.properties.id,
-                        data     : this.markers_data[d.properties.id] || {},
-                        has_data : !_.isEmpty(this.markers_data[d.properties.id])
-                    };
-                }
-            })
-            .filter(d => d);      
-            return markers;
-        },
         analytic_cluster_options() {
           return {
             onEachFeature: function(feature, layer) {
@@ -499,12 +371,6 @@ export default {
                 }, {permanent: false, direction: "center"});
             }
           };
-        },
-        popup_point_options(){
-            return {
-                minWidth : 300,
-                className: 'popupCustom'
-            };
         },
         heatmapLayer() {
 
@@ -649,23 +515,7 @@ export default {
           },
           deep: true
         },
-        geo_json(){
-            this.index.load(this.geo_json.features);
-            this.getClusterInfo();
-        },
-        markers(){
-            
-            if(this.center_default != this.center) return;
-            const total = this.markers.length;
-            if(!total) return this.center_default;
-            const markers_sum = this.markers.reduce ((acc,d)=>{
-                acc[0] = acc[0]+d.lat_lng[0];
-                acc[1] = acc[1]+d.lat_lng[1];
-                return acc;
-            },[0,0])
 
-            this.center = [markers_sum[0]/total,markers_sum[1]/total];
-        }
     },
     created(){
         
@@ -675,11 +525,7 @@ export default {
         this.center = this.center_default;
         this.getMapConfiguration();
         // Cuando geo_json es construido, se instancia Supercluster
-        this.index = new Supercluster({
-            radius: 40, // clusterizar en un radio de (radio es relativo al zoom)
-            maxZoom: 17 // Maximo zoom a clusterizar
-        });
-        this.index.load([]);
+
     },
     methods:{
         zoomToLocation(latLng){
@@ -1211,32 +1057,9 @@ export default {
             }
             return h;
         },
-        getPopupData(marker,col){
-            return (marker.data[col.id] === 'NULL') ? '-' : marker.data[col.id];
-        },
+
         setTileLayer(){
             this.url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        },
-        getMarkerData(marker){
-            const url = `${this.base_url}/entity/data/${this.entity_type_id}/${marker.id}?page=1`
-            axios.get(url)
-            .then((response) => {
-                try {
-                    
-                    let all_data = response.data.content;
-                    let marker_data = _.first(all_data.data) || {};
-                    this.$set(this.markers_data, marker.id, marker_data);
-                    
-                } catch (error) {
-                    console.error(error);
-                }
-            })
-            .catch((error) => {
-                console.error(error);
-            })
-            .finally(() => {
-                console.log('done data');
-            });
         },
         getMapConfiguration(){
             //data
@@ -1263,17 +1086,10 @@ export default {
             });
 
         },
-        getClusterInfo(){
-            
-            console.log('getClusterInfo');
-            
-            let bounds   = this.map.getBounds();
-            let bbox     = [bounds.getWest(), bounds.getSouth(), bounds.getEast(), bounds.getNorth()];      
-            let zoom     = this.map.getZoom();
-
-            let clusters_markers = this.index.getClusters(bbox, zoom);
-            
-            this.clusters_markers = clusters_markers;
+        onMapMoveEnd(){
+        
+            console.log('onMapMoveEnd');
+            this.$refs.analytic_cluster_layer.getClusterMarkers();
             
         },
         
@@ -1608,100 +1424,6 @@ export default {
         margin: 0 10px;
     }
 
-    .marker-cluster-small {
-        background-color: var(--sh-map-point-cluster-small-color);
-    }
-    .marker-cluster-small div {
-        background-color: var(--sh-map-point-cluster-small-color-div);
-        width:            var(--sh-map-point-cluster-small-size);
-        height:           var(--sh-map-point-cluster-small-size);
-        font:             var(--sh-map-point-cluster-small-font);
-        color:            var(--sh-map-point-cluster-small-font-color);
-        border-style:     var(--sh-map-point-cluster-small-border-style);
-        border-width:     var(--sh-map-point-cluster-small-border-width);
-        border-color:     var(--sh-map-point-cluster-small-border-color);
-    }
-
-    .marker-cluster-medium {
-        background-color: var(--sh-map-point-cluster-medium-color);
-    }
-    .marker-cluster-medium div {
-        background-color: var(--sh-map-point-cluster-medium-color-div);
-        width:            var(--sh-map-point-cluster-medium-size);
-        height:           var(--sh-map-point-cluster-medium-size);
-        font:             var(--sh-map-point-cluster-medium-font);
-        color:            var(--sh-map-point-cluster-medium-font-color);
-        border-style:     var(--sh-map-point-cluster-medium-border-style);
-        border-width:     var(--sh-map-point-cluster-medium-border-width);
-        border-color:     var(--sh-map-point-cluster-medium-border-color);
-    }
-
-    .marker-cluster-large {
-        background-color: var(--sh-map-point-cluster-large-color);
-    }
-    .marker-cluster-large div {
-        background-color: var(--sh-map-point-cluster-large-color-div);
-        width:            var(--sh-map-point-cluster-large-size);
-        height:           var(--sh-map-point-cluster-large-size);
-        font:             var(--sh-map-point-cluster-large-font);
-        color:            var(--sh-map-point-cluster-large-font-color);
-        border-style:     var(--sh-map-point-cluster-large-border-style);
-        border-width:     var(--sh-map-point-cluster-large-border-width);
-        border-color:     var(--sh-map-point-cluster-large-border-color);
-    }
-
-
-    .marker-cluster {
-        background-clip: padding-box;
-        border-radius: 20px;
-    }
-    .marker-cluster div {
-        margin-left: 5px;
-        margin-top: 5px;
-        text-align: center;
-        border-radius: 100%;
-    }
-    .marker-cluster span {
-        line-height: var(--sh-map-point-cluster-small-size);
-    }
-    .marker-cluster-medium span {
-        line-height: var(--sh-map-point-cluster-medium-size);
-    }
-    .marker-cluster-large span {
-        line-height: var(--sh-map-point-cluster-large-size);
-    }
-
-    .marker-pop-up-content{
-        max-height:350px;
-        overflow-y: scroll;
-    }
-    .marker-pop-up-content::-webkit-scrollbar {
-        width: 8px;
-        height: 8px;
-    }
-    .marker-pop-up-content::-webkit-scrollbar-thumb {
-        background: var(--sh-map-marker-pop-up-srcoll-color);
-        border-radius: 4px;
-    }
-
-    .marker-pop-up-content::-webkit-scrollbar-thumb:hover {
-        background: var(--sh-map-marker-pop-up-srcoll-color-hover);
-    }
-
-    .marker-pop-up-content::-webkit-scrollbar-thumb:active {
-        background-color: var(--sh-map-marker-pop-up-srcoll-color-active);
-    }
-
-
-    .marker-pop-up-single-info{
-        font:  var(--sh-map-marker-pop-up-content-font);
-        color: var(--sh-map-marker-pop-up-content-color);
-    }
-    .marker-pop-up-info-title{
-        font:  var(--sh-map-marker-pop-up-title-font);
-        color: var(--sh-map-marker-pop-up-title-color);
-
-    }
 
     .my-map >>> .leaflet-popup-content-wrapper,
     .my-map >>> .leaflet-popup-tip{
@@ -1720,6 +1442,15 @@ export default {
         gap: 8px;
         margin-top: 24px;
     }
+    
+    .my-map >>> .leaflet-popup-content-wrapper,
+    .my-map >>> .leaflet-popup-tip{
+        background-color: var(--sh-map-marker-pop-up-background);
+        border-color : var(--sh-map-marker-pop-up-border-color);
+        border-width : var(--sh-map-marker-pop-up-border-width);
+        border-style : var(--sh-map-marker-pop-up-border-style);
+    }
+
 
     .custom-controls .zoom-btn {
         background-color: var(--sh-map-zoom-button-background-color);
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/dist/'
-    : '/'
+    : '/',
+  devtool : 'source-map'
 })
